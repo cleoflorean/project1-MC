94a1c7599af20169c15f4a4d183dfffd21bb46c1
lsy-muthiasyahirah <muhiasyahirah@gmail.com>
2026-07-18 20:30:18 +0700

    bismillah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// 1. Tambahkan baris ini di bagian atas:
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // 2. Tambahkan kode ini di dalam fungsi boot:
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
