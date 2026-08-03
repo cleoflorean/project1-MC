9e47d08386c5de3f4b67d9dd0cc864f51325374b
lsy-muthiasyahirah <muhiasyahirah@gmail.com>
2026-08-03 08:19:23 +0700

    ----

--- database/migrations/2026_07_31_111015_add_komoditas_id_to_permintaans_table.php
+++ database/migrations/2026_07_31_111015_add_komoditas_id_to_permintaans_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::table('permintaans', function (Blueprint $table) {
             $table->foreignId('komoditas_id')->nullable()->constrained('komoditas')->onDelete('cascade');
-            $table->string('namatanaman', 30)->nullable()->change();
-            $table->string('komoditas', 20)->nullable()->change();
+            
+            // Hapus ->change() agar Laravel membuat ini sebagai kolom baru
+            $table->string('namatanaman', 30)->nullable();
+            $table->string('komoditas', 20)->nullable();
         });
     }
 
@@ -25,7 +27,10 @@ return new class extends Migration
     {
         Schema::table('permintaans', function (Blueprint $table) {
             $table->dropForeign(['komoditas_id']);
-            $table->dropColumn('komoditas_id');
+            
+            // Tambahkan kolom namatanaman dan komoditas di sini 
+            // agar ikut terhapus jika menjalankan php artisan migrate:rollback
+            $table->dropColumn(['komoditas_id', 'namatanaman', 'komoditas']);
         });
     }
 };
